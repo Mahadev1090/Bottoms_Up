fbe086ea1905eeae57d6e4831ec46cd7c729ef7b
DineshChoudhary <DineshChoudhary@AST-OL-200>
2024-01-11 22:32:41 +0530

    Validations for addMasterUser and AddMasterProject

--- src/main/java/com/ins/web/controller/MasterProjectController.java
+++ src/main/java/com/ins/web/controller/MasterProjectController.java
@@ -1,10 +1,13 @@
 package com.ins.web.controller;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,8 @@ import com.ins.web.service.MasterProjectService;
 import com.ins.web.vo.MasterProjectRequest;
 import com.ins.web.vo.MasterProjectVo;
 import com.ins.web.vo.MasterUserVo;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/masterProjects")
@@ -30,8 +35,23 @@ public class MasterProjectController {
 	}
     
 	@PostMapping("/addProject")
-    public ResponseEntity<MasterProjectVo> createProject(@RequestBody MasterProjectRequest masterProjectRequest) {
-        MasterProjectVo savedProject = masterProjectService.createProject(masterProjectRequest);
-        return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+    public ResponseEntity<Object> createProject(@RequestBody @Valid MasterProjectRequest masterProjectRequest, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+            // Build a custom error message or response
+            String errorMessage = "Invalid Input Data. Please check the provided fields.";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Collections.singletonMap("error", errorMessage));
+        }
+
+        try {
+        	ResponseEntity<Map<String, String>> savedProject = masterProjectService.createProject(masterProjectRequest);
+            return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // Handle other exceptions, log the error, and return an appropriate response
+            String errorMessage = "An unexpected error occurred.";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.singletonMap("error", errorMessage));
+        }
     }
 }
--- src/main/java/com/ins/web/dao/MasterProjectDao.java
+++ src/main/java/com/ins/web/dao/MasterProjectDao.java
@@ -1,9 +1,13 @@
 package com.ins.web.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ins.web.vo.MasterProjectVo;
 
 public interface MasterProjectDao extends JpaRepository<MasterProjectVo, Long> {
+
+	Optional<MasterProjectVo> findByProjectKey(Long projectKey);
 
 }
